24e24ef60ae63aaab31e792e74dfb3f4f68e5b39
Monica Jin <monicachenjin@gmail.com>
2024-04-22 10:23:11 +0000

    add: results from Elysium SmartFix and sGuard

---
+++ results/SmartFix/reentrancy/etherbank/candidates/cand_3/patch.sol
@@ -0,0 +1,35 @@
+contract SmartFix {
+  bool public _locked = false;
+
+  modifier _nonReentrant() {
+    require(!_locked);
+    _locked = true;
+    _;
+    _locked = false;
+  }
+}
+
+/*
+ * @source: https://github.com/seresistvanandras/EthBench/blob/master/Benchmark/Simple/reentrant.sol
+ * @author: -
+ * @vulnerable_at_lines: 21
+ */
+
+pragma solidity ^0.4.0;
+contract EtherBank is SmartFix {
+    mapping (address => uint) userBalances;
+    function getBalance(address user) constant returns(uint) {  
+		return userBalances[user];
+	}
+
+	function addToBalance() _nonReentrant /* <FIX> Add Modifier:NR */ {  
+		userBalances[msg.sender] += msg.value;
+	}
+
+	function withdrawBalance() _nonReentrant /* <FIX> Add Modifier:NR */ {  
+		uint amountToWithdraw = userBalances[msg.sender];
+        // <yes> <report> REENTRANCY
+		if (!(msg.sender.call.value(amountToWithdraw)())) { throw; }
+		userBalances[msg.sender] = 0;
+	}    
+}
