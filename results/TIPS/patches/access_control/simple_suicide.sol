729c14e262bb4099f27061b8e0c0d49acd59d3f1
sofiabobadilla <sofy.bobadilla@gmail.com>
2024-05-08 17:13:56 +0200

    Add: evaluation notebook

--- results/TIPS/patches/access_control/simple_suicide.sol
+++
@@ -1,7 +0,0 @@
-pragma solidity ^0.4.0;
-contract SimpleSuicide {
-	function sudicideAnyone() public {
-	selfdestruct(msg.sender);
-	}
-	
-}
--- results/TIPS/patches/time_manipulation/timed_crowdsale.sol
+++
@@ -1,7 +0,0 @@
-pragma solidity ^0.4.25;
-contract TimedCrowdsale {
-	function isSaleFinished() view public returns(bool ){
-	return block.timestamp >= 1546300800;
-	}
-	
-}
